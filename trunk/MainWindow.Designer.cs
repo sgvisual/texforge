729685d173634806f2d55a1e81e7cd9507326973
jocelyn.perreault@gmail.com <jocelyn.perreault@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-10-19 17:00:59 +0000

    first pass on the base ui

--- trunk/MainWindow.Designer.cs
+++ trunk/MainWindow.Designer.cs
@@ -28,20 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.RenderPreviewActive = new System.Windows.Forms.PictureBox();
+            this.RenderPreviewFull = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.GraphRender = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RenderPreviewActive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RenderPreviewFull)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphRender)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 313);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.GraphRender);
+            this.splitContainer1.Panel2.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(624, 289);
+            this.splitContainer1.SplitterDistance = 223;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.RenderPreviewActive);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.RenderPreviewFull);
+            this.splitContainer2.Size = new System.Drawing.Size(223, 289);
+            this.splitContainer2.SplitterDistance = 145;
+            this.splitContainer2.SplitterWidth = 1;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // RenderPreviewActive
+            // 
+            this.RenderPreviewActive.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RenderPreviewActive.Location = new System.Drawing.Point(0, 0);
+            this.RenderPreviewActive.Name = "RenderPreviewActive";
+            this.RenderPreviewActive.Size = new System.Drawing.Size(221, 143);
+            this.RenderPreviewActive.TabIndex = 0;
+            this.RenderPreviewActive.TabStop = false;
+            // 
+            // RenderPreviewFull
+            // 
+            this.RenderPreviewFull.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RenderPreviewFull.Location = new System.Drawing.Point(0, 0);
+            this.RenderPreviewFull.Name = "RenderPreviewFull";
+            this.RenderPreviewFull.Size = new System.Drawing.Size(221, 141);
+            this.RenderPreviewFull.TabIndex = 0;
+            this.RenderPreviewFull.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(397, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // GraphRender
+            // 
+            this.GraphRender.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GraphRender.Location = new System.Drawing.Point(0, 25);
+            this.GraphRender.Name = "GraphRender";
+            this.GraphRender.Size = new System.Drawing.Size(397, 264);
+            this.GraphRender.TabIndex = 1;
+            this.GraphRender.TabStop = false;
+            this.GraphRender.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphRender_Paint);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(624, 335);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.Text = "texforge";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.RenderPreviewActive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RenderPreviewFull)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphRender)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox RenderPreviewActive;
+        private System.Windows.Forms.PictureBox RenderPreviewFull;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.PictureBox GraphRender;
+
     }
 }
 
--- trunk/MainWindow.cs
+++ trunk/MainWindow.cs
@@ -11,9 +11,21 @@ namespace texforge
 {
     public partial class MainWindow : Form
     {
+        protected Graph graph;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            graph = new Graph();
+        }
+
+        private void GraphRender_Paint(object sender, PaintEventArgs e)
+        {
+            graph.Render(e.Graphics, e.ClipRectangle);
         }
     }
 }
