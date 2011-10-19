a09d19bafe8d72d55a5b50fce59c5664ecd996c7
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-10-19 17:02:59 +0000

    Added the definitions/settings project Added first iteration bitmap generation

---
+++ trunk/texforge-definitions/settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace texforge_definitions
+{
+    [Serializable]
+    public class settings
+    {
+        public int width = 512;
+        public int height = 512;
+
+    }
+}
