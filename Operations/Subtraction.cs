d366dd61355cba63ebbf0398523a52cdfd81369d
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-10-24 02:39:47 +0000

    Added some image operations, Blur, Multiply, Add, Subtract, Screen Added Generator base class for procedurally generated images (Sine waves, noise, etc)

--- Operations/Subtraction.cs
+++ Operations/Subtraction.cs
@@ -2,53 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Drawing;
 using System.Threading.Tasks;
 
 namespace texforge.Operations
 {
-    public class Addition : Operation
+    public class Subtraction : Operation
     {
         protected Atom operandA;
         protected Atom operandB;
 
+        public Subtraction(Atom a, Atom b)
+        {
+            operandA = a;
+            operandB = b;
+        }
+
         public enum eMode
         {
-            Add,
+            Subtract,
             Average
         }
-        eMode mode = eMode.Add;
+        eMode mode = eMode.Subtract;
+        
         public eMode Mode
         {
             get { return mode; }
             set { mode = value; }
         }
 
-        public Addition(Atom a, Atom b)
-        {
-            operandA = a;
-            operandB = b;
-        }
-
         public override Atom Execute()
-        {     
+        {
             byte[] bytesA = operandA.ToBytes();
             byte[] bytesB = operandB.ToBytes();
 
-            byte[] result = new byte[bytesB.Length];
+            byte[] result = new byte[bytesA.Length];
 
-            int bytes = bytesB.Length;
-            Parallel.For(0, bytes, index => 
-            { 
-                if ( mode == eMode.Average )
-                    result[index] = (byte)(Math.Min(((bytesA[index] + bytesB[index]) / 2), 255));
+            int bytes = bytesA.Length;
+            Parallel.For(0, bytes, index =>
+            {
+                if (mode == eMode.Average)
+                    result[index] = (byte)(Math.Max(((bytesA[index] - bytesB[index]) / 2), 0));
                 else
-                if ( mode == eMode.Add )
-                    result[index] = (byte)(Math.Min(((bytesA[index] + bytesB[index])), 255));
+                if (mode == eMode.Subtract)
+                    result[index] = (byte)(Math.Max(((bytesA[index] - bytesB[index])), 0));
+
             });
 
             return new Atom(result, operandA.Result.Size, operandA.Result.PixelFormat);
-                        
+
         }
+
+
+
     }
 }
