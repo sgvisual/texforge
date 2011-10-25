7c7fb2586e66ca78c8464007f94ebdf080657ee9
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-10-25 11:00:17 +0000

    Added some (work in progress) noise generation and some images for development and debugging.

--- trunk/Generators/Generator.cs
+++ trunk/Generators/Generator.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace texforge.Generators
 {
     public abstract class Generator
     {
+        public Generator(Size size, PixelFormat pixelFormat) 
+        { 
+            m_size = size;
+            m_pixelFormat = pixelFormat;
+        }
         public abstract Atom Generate();
+
+        protected Size m_size;
+        protected PixelFormat m_pixelFormat;
     }
 }
---
+++ trunk/Noise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace texforge
+{
+    public static class Noise
+    {
+        public static float Noise1(int x, int y)
+        {
+            int n = x + y * 57;
+            n = (n<<13)^n;
+            return ( 1f - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824f);   
+        }
+
+        public static float SmoothNoise(int x, int y)
+        {
+            float corners = (Noise1(x-1, y-1) + Noise1(x+1, y-1) + Noise1(x-1, y+1) + Noise1(x+1, y+1) ) / 16;
+            float sides = (Noise1(x-1, y) + Noise1(x+1, y) + Noise1(x, y-1) + Noise1(x, y+1) ) / 8;
+            float center = Noise1(x, y) / 4;
+
+            return corners + sides + center;
+        }
+        
+    }
+}
