a0c5f030450e8af9bd8335ca53f4130de0ce977a
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-10-26 16:55:18 +0000

    Added first pass serializable settings.

---
+++ trunk/texforge-definitions/FloatSetting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace texforge_definitions
+{
+    [SettingAttributes(CanRandomize=true)]
+    public class FloatSetting : Setting<float>
+    {
+        public FloatSetting(string name, float value, float defaultValue, float min, float max)
+            : base(name, value, defaultValue, min, max)
+        {
+        }
+
+        public override void RandomizeBetween(float min, float max)
+        {
+            if (!Attributes.CanRandomize)
+                return;
+
+            Random r = new Random();
+
+            value = min + (max * (float)r.NextDouble());
+            
+        }
+    }
+}
---
+++ trunk/texforge-definitions/SettingAttributes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace texforge_definitions
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class SettingAttributes : Attribute
+    {        
+
+        protected bool canRandomize = true;
+        public bool CanRandomize
+        {
+            get { return canRandomize; }
+            set { canRandomize = value; }
+        }
+
+        protected bool useLimits = false;
+        public bool UseLimits
+        {
+            get { return useLimits; }
+            set { useLimits = value; }
+        }
+
+    }
+}
