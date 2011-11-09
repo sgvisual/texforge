dd32b506e91e0635c4a00ee60ac320a7c42d4fea
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-11-09 17:48:04 +0000

    Changed transitions to connect between sockets

--- trunk/texforge-generator/Graph/Node.cs
+++ trunk/texforge-generator/Graph/Node.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace texforge.Graph
 {
-    public class Node
+    //[Serializable()]
+    public class Node //: ISerializable
     {
         public Node()
         {
@@ -28,6 +30,24 @@ namespace texforge.Graph
 
         protected List<Socket> inputSockets = new List<Socket>();
         protected List<Socket> outputSockets = new List<Socket>();
+
+        public Socket GetSocket(string name)
+        {
+            foreach (Socket s in inputSockets)
+            {
+                if (s.name.CompareTo(name) == 0)
+                    return s;
+            }
+
+            foreach (Socket s in outputSockets)
+            {
+                if (s.name.CompareTo(name) == 0)
+                    return s;
+            }
+
+            return null;
+        }
+        
         public class Socket
         {
             public enum Type
