788c5c14d6dce14eab1bc144979f408ebfbabe04
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-11-30 17:55:55 +0000

    Added TypeConverter to Color type to convert to and from string. To test, the value of the color in the ColorOverlay node is properly save/loaded.

--- trunk/texforge-definitions/Types/Color.cs
+++ trunk/texforge-definitions/Types/Color.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace texforge_definitions.Types
 {
+    [TypeConverter(typeof(Color.ColorTypeConverter))]
     public class Color
     {
         public byte red;
@@ -19,6 +21,60 @@ namespace texforge_definitions.Types
             this.blue = blue;
             this.alpha = alpha;
         }
+
+        public class ColorTypeConverter : TypeConverter
+        {
+            public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+            {
+                if (sourceType == typeof(string))
+                {
+                    return true;
+                }
+                return base.CanConvertFrom(context, sourceType);
+            }
+
+            public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+            {
+                if (value is string)
+                {
+                    string[] v = ((string)value).Split(new char[] { ',' });
+                    string r = v[0].ToUpper().Replace(@"R=", "");
+                    byte red = 0;
+                    byte.TryParse(r, out red);
+                    string g = v[1].ToUpper().Replace(@"G=", "");
+                    byte green = 0;
+                    byte.TryParse(g, out green);
+                    string b = v[2].ToUpper().Replace(@"B=", "");
+                    byte blue = 0;
+                    byte.TryParse(b, out blue);
+                    string a = v[0].ToUpper().Replace(@"A=", "");
+                    byte alpha = 0;
+                    byte.TryParse(a, out alpha);
+
+                    return new Color(red, green, blue, alpha);
+                }
+                return base.ConvertFrom(context, culture, value);
+            }
+
+            public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+            {
+
+                if (destinationType is string)
+                {
+                    Color color = (Color)value;
+                    return string.Format("r={0},g={1},b={2},a={3}", color.red, color.green, color.blue, color.alpha);
+                }
+
+                return base.ConvertTo(context, culture, value, destinationType);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("r={0},g={1},b={2},a={3}", red, green, blue, alpha);
+        }
+
+        
 
         public UInt32 ToUInt32()
         {
