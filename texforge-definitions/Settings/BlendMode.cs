958561127a9e30de0f5430d628f2997c06449878
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2011-12-02 17:49:22 +0000

    Added BlendMode setting

---
+++ texforge-definitions/Settings/BlendMode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace texforge_definitions.Settings
+{
+    public enum eBlendMode
+    {
+        None,
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Screen,
+        Overlay,
+        Dodge,
+        Burn
+    }
+
+    [SettingAttributes(CanRandomize=false)]
+    public class BlendMode : texforge_definitions.Setting<eBlendMode>
+    {
+
+        public BlendMode(string name, eBlendMode value, eBlendMode defaultValue)
+            : base(name, value, defaultValue)
+        {
+        }
+
+
+    }
+
+}
