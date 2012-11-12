1455e41c4b7a3750ff234a1ccadd1ed1df7fedec
lsempe@gmail.com <lsempe@gmail.com@626f24d7-c14c-d46a-af00-936183f28220>
2012-11-12 02:33:01 +0000

    Added initial support for texture operations

---
+++ texforge-generator/Graph/Nodes/Texture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace texforge.Graph.Nodes
+{
+    public class Texture : Node
+    {
+
+        public Texture(string name, string id, Graph graph)
+            : base(name, id, graph)
+        {
+            RegisterSocket(Socket.Type.Input, "In");
+            RegisterSocket(Socket.Type.Output, "Out");
+
+        }
+
+        public override object Process()
+        {
+            Atom inAtom = GetSocket("In").ConnectedAtom;
+            if (inAtom == null)
+            {
+                displayAtom = null;
+                GetSocket("Out").atom = null;
+                return null;
+            }
+
+            Atom result = new Atom(inAtom.Result, inAtom.Result.Size);
+
+            Graphics g = Graphics.FromImage(result.Result);
+
+            TextureBrush textureBrush = new TextureBrush(result.Result, System.Drawing.Drawing2D.WrapMode.Tile);
+            //textureBrush.ScaleTransform(0.25f, 0.25f);
+
+            // TODO: expose iterations, rotation, translation, scaling
+
+            Random random = new Random();
+            g.Clear(System.Drawing.Color.Transparent);
+            for (int i = 0; i < 10; ++i)
+            {
+                //textureBrush.ScaleTransform((float)random.NextDouble(), (float)random.NextDouble());
+                textureBrush.RotateTransform(360f * (float)random.NextDouble(), System.Drawing.Drawing2D.MatrixOrder.Prepend);
+                int x = (int)((1f + (2f * random.NextDouble())) * result.Result.Size.Width);
+                int y = (int)((1f + (2f * random.NextDouble())) * result.Result.Size.Height);
+
+                textureBrush.TranslateTransform(x, y);
+
+
+                g.FillRectangle(textureBrush, 0, 0, textureBrush.Image.Width, textureBrush.Image.Height);
+                g.Flush();
+            }
+
+            g.Flush();
+            g.Dispose();
+
+            displayAtom = result;
+            GetSocket("Out").atom = result;
+
+            return base.Process();
+        }
+    }
+}
